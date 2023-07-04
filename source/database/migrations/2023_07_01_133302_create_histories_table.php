5c1bd68e54dd4debabec3c0d1c0b55449b48f583
webmaster <webmaster@d02.npo-vdomain.com>
2023-07-04 15:55:11 +0900

    カレンダー追加

--- source/database/migrations/2023_07_01_133302_create_histories_table.php
+++ source/database/migrations/2023_07_01_133302_create_histories_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('histories', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('exercise_id')->constrained()->cascadeOnDelete();
+            $table->date('date');
+            $table->integer('sets');
+            $table->integer('reps');
+            $table->integer('weight');
             $table->timestamps();
         });
     }
--- source/routes/web.php
+++ source/routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Models\History;
+use App\Http\Controllers\HistoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,13 +16,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {return view('welcome');});
+Route::get('/', [HistoryController::class, 'index'])->middleware(['auth', 'verified'])->name('index');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', function () {return view('dashboard');})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
